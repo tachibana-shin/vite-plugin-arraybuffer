e68d7d81aa5bf5093c06a4c2c8e24690090d211c
Tachibana Shin <tachibshin@duck.com>
2024-02-23 18:08:59 +0000

    add watch file

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,26 @@ import { PluginOption } from "vite";
 export default function vitePluginArraybuffer(): PluginOption {
   return {
     name: "vite-plugin-arraybuffer",
+    resolveId(id) {
+      if (id.endsWith("?arraybuffer") || id.endsWith("?uint8array")) {
+        return id
+      }
+    },
     async transform(_src, id) {
       if (id.endsWith("?arraybuffer")) {
+        const file = id.slice(0, -12)
+        this.addWatchFile(file)
+
         return `export default new Uint8Array([
-          ${new Uint8Array(await promises.readFile(id.slice(0, -12)))}
+          ${new Uint8Array(await promises.readFile(file)).join(',')}
         ]).buffer`;
       }
       if (id.endsWith("?uint8array")) {
+        const file = id.slice(0, -11)
+        this.addWatchFile(file)
+
         return `export default new Uint8Array([
-          ${new Uint8Array(await promises.readFile(id.slice(0, -11)))}
+          ${new Uint8Array(await promises.readFile(file)).join(',')}
         ])`;
       }
 
