99ae1f32761f4aff6320862054f4768687eacfef
Len Boyette <boyettel@gmail.com>
2024-02-27 08:53:28 -0800

    use base64 instead of an integer array

--- src/main.ts
+++ src/main.ts
@@ -18,17 +18,23 @@ export default function vitePluginArraybuffer(): PluginOption {
         const file = id.slice(0, -12)
         this.addWatchFile(file)
 
-        return `export default new Uint8Array([
-          ${new Uint8Array(await promises.readFile(file)).join(',')}
-        ]).buffer`;
+        const buffer = await promises.readFile(file)
+        const b64 = buffer.toString('base64')
+
+        return `export default new Uint8Array(
+          Buffer.from('${b64}', 'base64')
+        ).buffer`
       }
       if (id.endsWith("?uint8array")) {
         const file = id.slice(0, -11)
         this.addWatchFile(file)
 
-        return `export default new Uint8Array([
-          ${new Uint8Array(await promises.readFile(file)).join(',')}
-        ])`;
+        const buffer = await promises.readFile(file)
+        const b64 = buffer.toString('base64')
+
+        return `export default new Uint8Array(
+          Buffer.from('${b64}', 'base64')
+        )`;
       }
 
       return;
